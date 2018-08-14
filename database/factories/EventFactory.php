b676c75bdf60e2d39b12cc776e9db8b9a94f6c10
dcoderk <webmarket2k@gmail.com>
2018-08-15 00:19:03 +0800

    updated eventfactory

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -4,9 +4,9 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Event::class, function (Faker $faker) {
     return [
-        'name'         =>   $faker->text(50),
-        'description'  =>   $faker->text(200),
-        'code'         =>   $fake->str_number(5),
-        'status'       =>   1 
+        'name'         => $faker->text(30),
+        'description'  => $faker->text(200),
+        'code'         => str_random(10),
+        'status'       => 1
     ];
 });
