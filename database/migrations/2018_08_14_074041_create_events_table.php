da9ee688803c476f10251a19563d5121f43f5c3e
dcoderk <webmarket2k@gmail.com>
2018-08-14 16:41:42 +0800

    Creating Events table and EeventsTableSeeder

---
+++ database/migrations/2018_08_14_074041_create_events_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->primary('id');
+            $table->string('name');
+            $table->text('description');
+            $table->string('code');
+            $table->integer('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
