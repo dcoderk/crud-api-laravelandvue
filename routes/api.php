841566e6d1dc586a5b03fc2da78f1fb4fa194f76
dcoderk <webmarket2k@gmail.com>
2018-08-15 23:30:35 +0800

    Creating EventController and Event Resource

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,19 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+// List events
+Route::get('events', 'EventController@index');
+
+// List single event
+Route::get('event/{id}','EventController@show');
+
+// Create new event
+Route::post('event','EventController@store');
+
+// Update event
+Route::put('event','EventController@store');
+
+// Delete event
+Route::delete('event/{id}', 'EventController@destroy');
